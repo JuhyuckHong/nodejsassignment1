69f7096b1578138e688af6d6f1a157dc0f4f4e30
Juhyuck-Hong <juhyukhong@gmail.com>
2023-04-23 21:01:53 +0900

    Add index page and MongoDB connection setup
    - Add a simple index page at the root path
    - Connect mongoDB using ./schemas module
    - Use express.json middleware for parsing Json data
    - Set the server to listen on port 3000

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const connect = require("./schemas");
+
+// mongoDB 연결
+connect();
+
+// 미들웨어 express 설정
+app.use(express.json())
+
+// 랜딩페이지
+app.get('/', (req, res) => {
+    res.send('test')
+});
+
+// 서버 실행
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port} now.`);
+});
