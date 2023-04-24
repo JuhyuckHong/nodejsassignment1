6a3937290dddab38c61528703156ff38e5e16d52
Juhyuck-Hong <juhyukhong@gmail.com>
2023-04-24 13:01:02 +0900

    refactor: 가독성을 위한 주석 추가 및 console.log 주석처리 삭제

--- routes/posts.js
+++ routes/posts.js
@@ -11,44 +11,43 @@ router.get("/posts", async (req, res) => {
 
 // 2. 게시글 작성
 router.post("/posts", async (req, res) => {
+    // body값을 변수에 저장
     const { user, password, title, content } = req.body
 
+    // 필수값인 user, password, title, content 중 없는 자료가 있는 경우
+    // 400, 데이터 형식 오류 메시지 반환
     if (!(user && password && title && content)) {
         return res.status(400).json({ message: "데이터 형식이 올바르지 않습니다." })
     }
-
+    // 가장 최근 부여된 postId 검색
     let lastPostId = await Posts.find({})
         .where('postId')
         .sort({ postId: -1 })
         .limit(1)
         .select("postId")
-    // console.log("test", lastPostId[0]["postId"])
-
     postId = lastPostId[0]["postId"] + 1
+    // 생성 시간 저장
     createdAt = new Date()
 
-    const createdPost = await Posts.create({
-        user,
-        password,
-        title,
-        content,
-        postId,
-        createdAt
-    })
-    // console.log("createdPost", createdPost)
+    // DB에 추가
+    await Posts.create({ user, password, title, content, postId, createdAt })
 
+    // 200, 게시글 생성 메시지 반환
     return res.status(200).json({ message: "게시글을 생성하였습니다." })
 
 })
 
 // 3. 게시글 상세 조회
 router.get("/posts/:_postId", async (req, res) => {
+    // params를 _postI로 저장
     const { _postId } = req.params
 
+    // postId가 _postId같은 경우, _id, __v, password를 뺀 데이터를 반환
     await Posts.find({})
         .where("postId").equals(_postId)
         .select("-_id -__v -password").then((result) => {
             return res.status(200).json({ "data": result })
+            // postId 필드 타입과 다른 경우 400, 데이터 형식 오류 메시지 반환
         }).catch((error) => {
             return res.status(400).json({ message: "데이터 형식이 올바르지 않습니다." })
         })
