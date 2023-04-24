17a500e6e5cfd5185cdb298addcf1190ee37f605
Juhyuck-Hong <juhyukhong@gmail.com>
2023-04-24 12:50:38 +0900

    feat: 게시글 수정 API 추가
    - /posts/:_postId PUT 요청에 대해 DB에서 해당 게시글을 검색해서 body에 담긴 자료로 업데이트
    - postId로 검색 시 결과가 없는 경우 404 반환, 게시글 조회 실패 메시지 전달
    - _postId 파라미터가 Posts스키마 postId필드 타입과 다른 경우 400반환,  데이터 형식이 올바르지 않음 메시지 전달

--- routes/posts.js
+++ routes/posts.js
@@ -57,7 +57,29 @@ router.get("/posts/:_postId", async (req, res) => {
 
 // 4. 게시글 수정
 router.put("/posts/:_postId", async (req, res) => {
+    // 파라미터로 들어온 _postId를 필드 타입인 Number로 변환해서 저장하고,
+    const _postId = Number(req.params._postId)
+    // 수정 할 내용이 담긴 body에서, password와 title, contents를 받아와서
+    const { password, title, content } = req.body
 
+    try {
+        // 게시글 내용을 DB에서 검색해서 result로 반환
+        const result = await Posts.find({})
+            .where("postId").equals(_postId)
+            .select("-_id -__v -user")
+
+        // 검색결과가 있는 경우 post에 저장
+        if (result.length) {
+            await Posts.updateOne({ postId: _postId }, { $set: { title, content, password } })
+            return res.status(200).json({ message: "게시글을 수정하였습니다." })
+            // 검색 결과가 없는 경우 400, 게시글 조회 실패 메시지 반환
+        } else {
+            return res.status(404).json({ message: "게시글 조회에 실패하였습니다." })
+        }
+        // 필드 타입 Number로 들어오지 않은 경우 400, 데이터 형식 오류 메시지 반환
+    } catch (error) {
+        return res.status(400).json({ message: "데이터 형식이 올바르지 않습니다." })
+    }
 })
 
 // 5. 게시글 삭제 
